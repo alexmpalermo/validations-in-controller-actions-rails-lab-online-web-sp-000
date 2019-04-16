635145eae600b792d4973ef0829469532678e499
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-16 00:13:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, 
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :content, 
 end
